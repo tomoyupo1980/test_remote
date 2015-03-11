cbe204c59bcb7b12a8ff8e361b2b27f5fa676fdb
tomoyupo <tomoyupo@gmail.com>
2015-03-11 10:30:22 +0900

    aaa

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,7 +2,6 @@
 public class HelloGit {
 public static void main(String[] args) {
 	System.out.println("gitã¯ã˜ã‚ã¾ã™ãŸã€‚");
-	System.out.println("gitã¯ã˜ã‚ã¾ã™ãŸã€‚");
 	//‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚ ‚Ü[‚¶‚µ‚½‚ ‚ ‚ ‚ ‚ ‚ aa
 	//ƒRƒƒ“ƒg’Ç‰ÁZZ
 }
